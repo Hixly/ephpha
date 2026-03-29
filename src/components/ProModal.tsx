b2d53d600fe4674189c5241369cc00c1864c3258
Ephpha <ephpha.ai@yahoo.com>
2026-03-28 23:38:25 -0400

    Delete src/components/ProModal.tsx

--- src/components/ProModal.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react'
-
-interface ProModalProps {
-  onClose: () => void
-}
-
-export default function ProModal({ onClose }: ProModalProps) {
-  const [activeTab, setActiveTab] = useState
