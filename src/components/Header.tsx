4d56b12e9472318a07551bc0cfb87ecc6a6bdf7c
Ephpha <ephpha.ai@yahoo.com>
2026-03-28 22:12:11 -0400

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+interface HeaderProps {
+  onSettingsClick: () => void
+  onHistoryClick: () => void
+}
+
+export default function Header({ onSettingsClick, onHistoryClick }: HeaderProps) {
+  return (
